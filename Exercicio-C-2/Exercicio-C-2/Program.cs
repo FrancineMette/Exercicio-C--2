b679790d3e6d6a67247a78397d2e72012a60291f
FrancineMette <francineisabelsantos@hotmail.com>
2025-02-17 17:58:26 -0300

    teste

---
+++ Exercicio-C-2/Exercicio-C-2/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+
+    static void Main()
+    {
+        Console.Write("Digite seu nome: ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Digite seu sobrenome: ");
+        string sobrenome = Console.ReadLine();
+           
+        string mensagem = $"Olá, {nome + " " + sobrenome}! Seja muito bem-vindo!";
+        
+        Console.WriteLine(mensagem);
+        
+     }
+}
